776118b160dd3016f07d6f3e54f103a2240083e1
Micho Todorovich <micho.todorovich@gmail.com>
2017-11-18 18:54:44 -0600

    removed unused includes

--- Source/Intrepid/Status Indicator/StatusIndicatorWidgetStyle.cpp
+++ Source/Intrepid/Status Indicator/StatusIndicatorWidgetStyle.cpp
@@ -2,16 +2,6 @@
 
 #include "StatusIndicatorWidgetStyle.h"
 #include "Status Indicator/StatusIndicator.h"
-//#include "Slate/SlateBrushAsset.h"
-//#include "Intrepid.h"
-//#include "Materials/MaterialInstanceDynamic.h"
-//#include "Materials/MaterialInstance.h"
-//#include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
-//#include "Runtime/Engine/Classes/Engine/Texture2D.h"
-//#include "AssetRegistryModule.h"
-//#include "ARFilter.h"
-//#include "DataSingleton.h"
-
 
 const FName FStatusIndicatorStyle::TypeName(TEXT("FStatusIndicatorStyle"));
 
